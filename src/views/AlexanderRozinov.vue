b3c8441fabd2a730827cda8c24af0793a30967a0
Evelin <teakid21@gmail.com>
2022-08-01 21:49:28 +0200

    Alex Rovinov's biography added

--- src/views/AlexanderRozinov.vue
+++ src/views/AlexanderRozinov.vue
@@ -1,13 +1,46 @@
 <template>
 	<main>
 		<h1>Alexander Rozinov</h1>
-		<p class="warning">En proceso...</p>
+		<h3 class="title">Ilustración Digital | Música Electrónica</h3>
+		<p>
+			Comencé a dibujar como tantos de mi edad: copiando dibujos de Pokémon, pintando con acuarelas sin saber
+			usarlas, viendo dibujos animados y pensando "ojalá un día hacer algo tan bonito".
+		</p>
+		<p>Por eso mismo descubrir internet fue lo más importante en mi trayectoria como artista.
+			Ver lo que creaban los usuarios de páginas como Newgrounds o Deviantart, y ver que esta era gente normal como
+			yo, sin tener un grán estudio o empresa de por medio, fue la mayor revelación e inspiración para un crío como
+			lo era yo.
+			De un día para otro pasé a ser un adolescente y me propuse a tomar esto de dibujar un poco más en serio.</p>
+		<p>
+			Aunque lo que hago son ilustraciones de personajes, mi intención es crear imágenes que se sientan redondas
+			tanto a nivel estético como temático, y transmitir emoción mediante la presentación misma, algo que de un solo
+			vistazo cree una fuerte impresión.
+			En este último año, lo que más me ha interesado es reforzar mis composiciones mediante la sinergía del diseño
+			gráfico y las paletas de colores definidos y saturados.
+		</p>
+
+		<p>
+			Por el camino también me interesé por la creación de música, y rápidamente se convirtió en mi segunda grán
+			pasión.<br/>
+			Bajo el nombre de 'Nechromatic', lo que hago es experimentar, explorar sonidos y texturas y crear espacios
+			mediante la manipulación electrónica.
+			Composiciones densas con capas y capas de efectos, collages de audio, ambient psicodélico y ritmos dispares.
+			Cuanto más descabellado mejor.
+			En mi primer EP, <a href="https://www.youtube.com/watch?v=6t-Ri6DpWng" target="_blank">'World Wide Web'</a>, he
+			puesto en práctica las cosas que he aprendido a lo largo de los años con
+			mis experiencias en el internet de la década de los 2000 como ancla temático.
+		</p>
+
+		<p>
+			¿El próximo paso? Crear una ilustración que genere música. ¿O una canción que vaya a generar una ilustración?
+		</p>
+
 	</main>
 </template>
 
 <style scoped>
 main {
-	padding: 0 var(--margin);
+	padding: 0 var(--margin) 2rem var(--margin);
 }
 
 
