12bb4654c1a34fa772e05fa95203e1ebdba4c6ab
Evelin <teakid21@gmail.com>
2022-08-07 19:57:12 +0200

    Mikel's bio

--- src/views/MikelSarrias.vue
+++ src/views/MikelSarrias.vue
@@ -1,7 +1,48 @@
 <template>
 	<main>
 		<h1>Mikel Sarrias</h1>
-		<p class="warning">En proceso...</p>
+		<h3 class="title">Ilustrador</h3>
+		<p>
+			Siempre he sentido una gran afición por los cómics y la ilustración en general pero no fue hasta que me metí en
+			el <span class="bold">Grado Superior de la Escuela de Artes y Superior de Diseño de Pamplona</span> que empecé
+			a enfocarme en ello de
+			una manera más seria.
+		</p>
+		<p>
+			Desde siempre he preferido ilustrar criaturas monstruosas, armaduras, cascos, máscaras, robots y generalmente
+			todo lo que tuviera cierto carácter <span class="bold">bélico</span>, pero a pesar de eso en la actualidad me e
+			empezado a centrar en
+			dibujar a personas, pero lo que hago por lo general es <span class="bold">deshumanizarlos</span> con el uso del
+			claroscuro, reduciendo
+			en gran parte la expresividad del personaje y evitando así empatizar con la figura como si de una máscara se
+			tratara.
+		</p>
+		<p>
+			Uno de los puntos que más característiza mis ilustraciones serían el estilo que puede variar hacia un lado algo
+			más <span class="bold">cartoon</span> y simple o otro más similar al <span class="bold">cómic</span> y con un
+			mayor nivel de detalle, pero en general ambos tienen
+			un nivel de iconicidad bajo, apesar de todo tambien me gusta variar de estilos.
+		</p>
+		<p>
+			Tanto el trazo como el uso del color también serían dos puntos a destacar, puesto que uso un <span
+			class="bold">trazo muy
+			definido</span>, y en general limpio además de que denota mucha expresividad.
+			En cuanto al color, mi <span class="bold">paleta principal es bastante reducida</span>, esencialmente se
+			compone de negro, rojo y blanco.
+			Aunque a pesar de ello también trabajó con una variedad cromática más amplia pero la gran mayoría de veces
+			usando <span class="bold">colores planos</span>.
+		</p>
+		<p>
+			Mis principales fuentes de inspiración y una de las cosas que más me motiva a la hora de ilustrar es la
+			<span class="bold">mitología</span>, tengo interés en todas pero de las que más conocimiento tengo serían de la
+			<span class="bold">griega, nórdica y
+				japonesa</span>. También la <span class="bold">historia</span> en general me resulta muy interesante puesto
+			que una de las cosas que más me gusta representar son escenas bélicas, o relacionadas con ese tipo de temas.
+			<br/>
+			<span class="bold">El cine</span> también me ha marcado mucho a la hora de ilustrar, mostrándome la importancia
+			de la iluminación, la composición de la imagen y el uso del color para representar significados concretos.
+
+		</p>
 	</main>
 
 
@@ -9,7 +50,7 @@
 
 <style scoped>
 main {
-	padding: 0 var(--margin);
+	padding: 0 var(--margin) var(--margin) var(--margin);
 }
 
 
